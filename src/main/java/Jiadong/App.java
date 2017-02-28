55e90dbcad257ef49e990f63d945c580a28dd7f2
Wang Jiadong <wang_jiad@worksap.co.jp>
2017-02-28 16:46:32 +0800

    init

---
+++ src/main/java/Jiadong/App.java
@@ -0,0 +1,13 @@
+package Jiadong;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
