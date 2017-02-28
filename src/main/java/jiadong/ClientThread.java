8403a8d8614a7cdc94e921310b0a71398376f1c3
Wang Jiadong <wang_jiad@worksap.co.jp>
2017-02-28 19:35:27 +0800

    lalalal

---
+++ src/main/java/jiadong/ClientThread.java
@@ -0,0 +1,13 @@
+package jiadong;
+
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+	private Socket client; 
+	ClientThread(Socket client){
+		this.client = client;
+	}
+	public void run(){
+		
+	}
+}
