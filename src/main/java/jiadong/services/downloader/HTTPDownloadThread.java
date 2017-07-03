729b1653c915820263d5dd4bee438c7f493d39f0
Wang Jiadong <wangjiadong1993@gmail.com>
2017-07-03 21:05:40 +0800

    added all

--- src/main/java/jiadong/services/downloader/HTTPDownloadThread.java
+++ src/main/java/jiadong/services/downloader/HTTPDownloadThread.java
@@ -7,26 +7,29 @@ import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.net.ssl.SSLSocketFactory;
-
 import jiadong.workers.network.Request;
 
-public class HTTPDownloadThread implements Runnable{
+public class HTTPDownloadThread implements Runnable {
 	private Request request;
 	private Collector collector;
 	private DownloadStatus status = DownloadStatus.NONE;
-	
-	public HTTPDownloadThread(Request r, Collector c){
+
+	public HTTPDownloadThread(Request r, Collector c) {
 		this.request = r;
 		this.collector = c;
 		this.status = DownloadStatus.INITIALIZED;
 	}
-	public void setRequest(Request r){
+
+	public void setRequest(Request r) {
 		this.request = r;
 		this.status = DownloadStatus.INITIALIZED;
 	}
-	public Request getRequest(){return this.request;}
+
+	public Request getRequest() {
+		return this.request;
+	}
+
 	@Override
 	public void run() {
 		try {
@@ -36,18 +39,19 @@ public class HTTPDownloadThread implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	public void startDownload() throws IOException{
+
+	public void startDownload() throws IOException {
 		Socket socket;
 		OutputStream os;
 		InputStream is;
 		this.status = DownloadStatus.DOWNLOADING;
-		if(this.request.protocolType.equals("HTTP")){
+		if (this.request.protocolType.equals("HTTP")) {
 			System.setProperty("javax.net.ssl.trustStore", "clienttrust");
 			SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
 			socket = ssf.createSocket(this.request.host, Integer.parseInt(this.request.port));
-		}else if(this.request.protocolType.equals("HTTPS")){
+		} else if (this.request.protocolType.equals("HTTPS")) {
 			socket = new Socket(this.request.host, Integer.parseInt(this.request.port));
-		}else{
+		} else {
 			return;
 		}
 		os = socket.getOutputStream();
@@ -62,37 +66,37 @@ public class HTTPDownloadThread implements Runnable{
 		int tmp_int = 0;
 		int maxLeng = 1024;
 		byte[] buffer = new byte[maxLeng];
-		List<Byte>  bufferLinkedList = new ArrayList<>();
+		List<Byte> bufferLinkedList = new ArrayList<>();
 		String header = null;
-		int headerContentLength=-1;
+		int headerContentLength = -1;
 		try {
-//			System.out.println("=======Start==================");
-			while((tmp_int = is.read(buffer)) != -1){
-				
-//				reading the data into arrayList;
-				System.out.println(" "+System.currentTimeMillis() + " read: " + tmp_int +" bytes.");
-				for(int i=0;i<tmp_int; i++){
+			// System.out.println("=======Start==================");
+			while ((tmp_int = is.read(buffer)) != -1) {
+
+				// reading the data into arrayList;
+				System.out.println(" " + System.currentTimeMillis() + " read: " + tmp_int + " bytes.");
+				for (int i = 0; i < tmp_int; i++) {
 					bufferLinkedList.add(new Byte(buffer[i]));
 				}
-				if(tmp_int != maxLeng){
-					if(header == null){
+				if (tmp_int != maxLeng) {
+					if (header == null) {
 						int tmp = getHeaderEnd(bufferLinkedList);
-						if(tmp == -1){
+						if (tmp == -1) {
 							continue;
-						}else{
+						} else {
 							header = this.getHeaderString(bufferLinkedList.subList(0, tmp));
 							bufferLinkedList = bufferLinkedList.subList(tmp, bufferLinkedList.size());
 							headerContentLength = this.getHeaderContentLength(header);
-							if(headerContentLength == bufferLinkedList.size()){
+							if (headerContentLength == bufferLinkedList.size()) {
 								break;
-							}else{
+							} else {
 								continue;
 							}
 						}
-					}else{
-						if(headerContentLength == bufferLinkedList.size()){
+					} else {
+						if (headerContentLength == bufferLinkedList.size()) {
 							break;
-						}else{
+						} else {
 							continue;
 						}
 					}
@@ -101,44 +105,46 @@ public class HTTPDownloadThread implements Runnable{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if(bufferLinkedList.size() == headerContentLength){
+		if (bufferLinkedList.size() == headerContentLength) {
 			this.collector.sendData(this.request, byteListToArray(bufferLinkedList));
-		}else{
-//			register as bad;
+		} else {
+			// register as bad;
 		}
 		socket.close();
 		this.status = DownloadStatus.NONE;
 		collector.register(this);
 	}
-	private int getHeaderEnd(List<Byte>  byteList){
-		for(int i=0; i< byteList.size()-3; i++){	
-			if(byteList.get(i).intValue() == ('\r'-0) && 
-				byteList.get(i+1).intValue() == ('\n'-0) &&
-				byteList.get(i+2).intValue() == ('\r'-0) &&
-				byteList.get(i+3).intValue() == ('\n'-0)){
-				return i+4;
+
+	private int getHeaderEnd(List<Byte> byteList) {
+		for (int i = 0; i < byteList.size() - 3; i++) {
+			if (byteList.get(i).intValue() == ('\r' - 0) && byteList.get(i + 1).intValue() == ('\n' - 0)
+					&& byteList.get(i + 2).intValue() == ('\r' - 0) && byteList.get(i + 3).intValue() == ('\n' - 0)) {
+				return i + 4;
 			}
 		}
 		return -1;
 	}
-	private byte[] byteListToArray(List<Byte>  byteList){
+
+	private byte[] byteListToArray(List<Byte> byteList) {
 		byte[] tmp = new byte[byteList.size()];
-		for(int i=0; i<tmp.length; i++)
+		for (int i = 0; i < tmp.length; i++)
 			tmp[i] = byteList.get(i);
 		return tmp;
 	}
-	private String getHeaderString(List<Byte>  bufferLinkedList){
+
+	private String getHeaderString(List<Byte> bufferLinkedList) {
 		byte[] tmp = byteListToArray(bufferLinkedList);
 		try {
-			return new String(tmp,0, tmp.length , "ISO-8859-1");
+			return new String(tmp, 0, tmp.length, "ISO-8859-1");
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
 	}
-	private int getHeaderContentLength(String header){
+
+	private int getHeaderContentLength(String header) {
 		int contentLengthIndex = header.indexOf("Content-Length:");
 		String subString = header.substring(contentLengthIndex);
-		subString = subString.substring(subString.indexOf(' ')+1, subString.indexOf('\r'));
+		subString = subString.substring(subString.indexOf(' ') + 1, subString.indexOf('\r'));
 		return Integer.parseInt(subString);
 	}
 
